77bfa5a599c29d38f34ef28803a3e9e945aa6c0e
kevminh <kevinminh.ng@gmail.com>
2021-09-22 15:03:34 +0200

    another commit

--- Portfolio website/assets/js/main.js
+++ Portfolio website/assets/js/main.js
@@ -98,3 +98,33 @@ tabs.forEach(tab => {
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+//We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () => document.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+// we validate if the user previously chose a topic
+if (selectedTheme) {
+    //IF the validation is fulfilled, we ask what the issue was to know if we activated or deactived the dark theme
+    document.body.classList[selectedtheme === "dark" ? "add" : "remove"](darkTheme);
+    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](iconTheme);
+};
+
+// Active / Deactive the theme manualla with the button
+themeButton.add("click", () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //We save the theme and the current icon that the user chose
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+})
+
