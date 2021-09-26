bfda387e54d7cd0a10d1cd2fd111f6addb88e8cb
Kevin Velaz <56735050+Kevinminh@users.noreply.github.com>
2021-09-26 14:10:54 +0200

    26/sept commit

--- Portfolio website/assets/js/main.js
+++ Portfolio website/assets/js/main.js
@@ -103,7 +103,21 @@ modalCloses.forEach((modalClose) => {
     })
 });
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".swiper-container", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+        nextEL: ".swiper-button-next",
+        prevEL: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+})
 
 /*==================== TESTIMONIAL ====================*/
 
